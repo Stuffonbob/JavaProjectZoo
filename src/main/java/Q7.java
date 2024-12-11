4801dbcb0a7f5109fb919a4d4c2eb9a386d9b6ed
ziad-cpu <hziademp@gmail.com>
2024-12-10 23:18:10 -0500

    q7

--- src/main/java/Q7.java
+++ src/main/java/Q7.java
@@ -1,5 +1,20 @@
 /*Count the Number of Words in a String: Write a function to count
 the number of words in a given string. Words are separated by spaces or punctuation.
 For example, the input "Hello, world!" should return 2.*/
-public class Q7 {
+
+class WordCounter {
+    public static int countWordsInString(String inputString) {
+        String[] words = inputString.split("\\W+");
+        return words.length;
+    }
+
+    public class Q7 {
+        public static void main(String[] args) {
+            String inputString = "Elephant, giraffe and zebra are in the zoo!";
+            int wordCount = countWordsInString(inputString);
+            System.out.println("The number of words in the input string is: " + wordCount);
+
+        }
+    }
 }
+
