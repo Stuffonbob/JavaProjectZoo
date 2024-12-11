04aa1d77b4c6357c99887be4faef8c7d234d556c
ziad-cpu <hziademp@gmail.com>
2024-12-10 23:13:00 -0500

    q9

--- src/main/java/Q9.java
+++ src/main/java/Q9.java
@@ -1,4 +1,37 @@
+import java.util.HashMap;
+
 /*Find the First Non-Repeating Character in a String: Given a string, find and return the first non-repeating character.
 For example, in the string "abracadabra", the first non-repeating character is 'c'.*/
-public class Q9 {
+class FirstNonRepeatingCharacter {
+
+    public static char findFirstNonRepeatingCharacter(String inputString) {
+        HashMap<Character, Integer> charCountMap = new HashMap<>();
+
+        for (int i = 0; i < inputString.length(); i++) {
+            char currentChar = inputString.charAt(i);
+            charCountMap.put(currentChar, charCountMap.getOrDefault(currentChar, 0) + 1);
+        }
+
+        for (int i = 0; i < inputString.length(); i++) {
+            char currentChar = inputString.charAt(i);
+            if (charCountMap.get(currentChar) == 1) {
+                return currentChar;
+            }
+        }
+        return '\0';
+    }
+
+
+    public class Q9 {
+        public static void main(String[] args) {
+            String inputString = "elephant";
+            char firstNonRepeatingChar = findFirstNonRepeatingCharacter(inputString);
+            if (firstNonRepeatingChar != '\0') {
+                System.out.println("The first non-repeating character is: " + firstNonRepeatingChar);
+            } else {
+                System.out.println("No non-repeating character found.");
+            }
+        }
+    }
 }
+
